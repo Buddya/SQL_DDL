8bc6762dc2689711e40aa8c26248cc569b1dadda
Buddya <tsebekov94@gmail.com>
2022-06-08 04:59:21 +0300

    Initial commit

---
+++ create_table.sql
@@ -0,0 +1,9 @@
+create table PERSONS
+(
+    name varchar(255)  not null,
+    surname varchar(255)  not null,
+    age int check (PERSONS.age > 0),
+    phone_number int,
+    city_of_living varchar(255),
+    primary key (name, surname, age)
+);
